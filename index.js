f4e916c21b00d757815892112d49829024fcfa1c
qodirovdoston806 <qodirovdoston806@gmail.com>
2023-05-15 15:07:57 +0500

    fdf

--- index.js
+++ index.js
@@ -18,4 +18,4 @@ function blurring() {
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-};ZZZZZZZZZZZZZZZZZyyyyyyyyy11lllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll`q   `
+};
